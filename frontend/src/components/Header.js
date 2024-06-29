309a179f4ca23cfa9bbcf5ffd4542daa88e4d858
Sajid <mdsajidali1803@gmail.com>
2024-06-29 13:28:45 +0530

    Add header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Logo from './Logo';
+
+const Header = () => {
+  return (
+
+    <header className='h-16 shadow-md'>
+      
+      <div className=" h-full container mx-auto flex items-center px-4 justify-between">
+
+        <div className=''>
+          <Logo w={90} h={50}/>
+        </div>
+
+        <div>
+          <input type="text" placeholder='search product here...'/>
+        </div>
+
+        <div>
+          user icons and card
+        </div>
+
+      </div>
+      
+    </header>
+
+  );
+
+};
+
+export default Header;

15b7c35c017d2a85ebb30e59e75aee804cecf9a2
Sajid <mdsajidali1803@gmail.com>
2024-06-30 19:22:28 +0530

    Initialized Express.js server with CORS, database connection, and environment variables.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
-// Set up Express.js server with CORS enabled, configured environment variables, and established a default port for server listening.
+// "Set up Express.js server with CORS, connected to MongoDB database using dotenv for environment variables, and established a dynamic port for server listening."
 
 //  summarizes the main tasks performed in the code, which include:-
 // (i) Setting up an Express.js server
 // (ii) Enabling CORS (Cross-Origin Resource Sharing)
 // (iii) Configuring environment variables using dotenv
 // (iv) Establishing a default port for the server to listen on
+//  (v) Connecting to a MongoDB database using the connectDB function
+// (vi) Establishing a dynamic port for the server to listen on
 
 
 
@@ -12,6 +14,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/db')
 
 
 const app = express();
@@ -19,6 +22,9 @@ app.use(cors());
 
 const PORT = 8080 || ProcessingInstruction.env.PORT;
 
-app.listen(PORT, () => {
-    console.log("Server is running");
+connectDB()?.then(()=> {
+    app.listen(PORT, () => {
+        console.log("Server is running");
+    });
 });
+

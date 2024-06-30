5c6d8fbc8936e0fc75b980d42414b02ab95bc9b7
Sajid <mdsajidali1803@gmail.com>
2024-06-30 12:55:09 +0530

    Add updated footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const Footer = () => {
+
   return (
-    <div>
-      Footer
-    </div>
+
+    <footer className='bg-slate-200'>
+
+      <div className='container mx-auto p-4'>
+        <p className='text-center font-bold' title='I am fine.'>Hello! How are you?</p>
+      </div>
+
+    </footer>
+
   );
+
 };
 
 export default Footer;

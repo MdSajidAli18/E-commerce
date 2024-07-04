d0fdad62ed28ed776b8d086797231bcd01aaefb5
Sajid <mdsajidali1803@gmail.com>
2024-07-04 19:40:43 +0530

    Add updated files

--- backend/index.js
+++ backend/index.js
@@ -11,20 +11,31 @@
 
 
 
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
 const connectDB = require('./config/db')
+const router = require('./routes')
 
 
-const app = express();
-app.use(cors());
 
-const PORT = 8080 || ProcessingInstruction.env.PORT;
+const app = express()
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+app.use(express.json({limit: '50mb'})) // limit is set for user profilePic.(I have resolved this issue.)
+app.use(cookieParser())
+
+app.use("/api", router)
+
+const PORT = 8080 || Process.env.PORT;
 
 connectDB()?.then(()=> {
     app.listen(PORT, () => {
         console.log("Server is running");
     });
 });
+
 

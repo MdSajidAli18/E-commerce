61ff4be8f9e0fbc33ccbcc60c8c833b9df2c9c69
Sajid <mdsajidali1803@gmail.com>
2024-07-06 20:45:01 +0530

    Add updated file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,8 @@ const userDetailsController = require('../controller/userDetails')
 const authToken = require('../middleware/authToken')
 const userLogout = require('../controller/userLogout')
 const allUsers = require('../controller/allUsers')
+const updateUser = require('../controller/updateUser')
+
 
 router.post("/signup", userSignUpController)
 router.post("/signin", userSignInController)
@@ -16,7 +18,7 @@ router.get("/userLogout", userLogout)
 
 // Admin Panel
 router.get("/all-users",authToken, allUsers)
-
+router.post("/update-user", authToken, updateUser)
 
 
 module.exports = router

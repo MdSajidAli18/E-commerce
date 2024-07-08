afdc10619408f31fb6dcd7e99fac3263c61c684f
Sajid <mdsajidali1803@gmail.com>
2024-07-08 21:24:51 +0530

    Add updated files

--- frontend/src/components/UploadProduct.js
+++ frontend/src/components/UploadProduct.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { CgClose } from "react-icons/cg";
 import productCategory from '../helpers/productCategory';
 import { FaCloudUploadAlt } from "react-icons/fa";
+import uploadImage from '../helpers/uploadImage';
+import DisplayImage from './DisplayImage';
+import { MdDelete } from "react-icons/md";
 
 const UploadProduct = ({
     onClose
@@ -11,13 +14,57 @@ const UploadProduct = ({
         productName: "",
         brandName: "",
         category: "",
-        productImage: "",
+        productImage: [],
         description: "",
         price: "",
-        selling: ""
+        sellingPrice: ""
     })
 
-    const handleOnChange = (e)=>{}
+    const [openFullScreenImage, setOpenFullScreenImage] = useState(false)
+    const [fullScreenImage, setFullScreenImage] = useState("")
+
+    const handleOnChange = (e)=>{
+        const {name, value} = e.target
+
+        setData( (prev)=>{
+            return{
+                ...prev,
+                [name]: value
+
+            }
+        })
+    }
+
+
+    const handleUploadProduct = async(e)=>{
+        const file = e.target.files[0]
+        const uploadImageCloudinary = await uploadImage(file)
+
+        setData( (prev)=>{
+            return{
+                ...prev,
+                productImage: [...prev.productImage, uploadImageCloudinary.url]
+
+            }
+        })
+        console.log("upload image on cloudinary", uploadImageCloudinary.url);
+    }
+
+
+    const handleDeleteProductImage = async(index)=>{
+        console.log("Image index", index);
+
+        const newProductImage = [...data.productImage]
+        newProductImage.splice(index, 1)
+
+        setData( (prev)=>{
+            return{
+                ...prev,
+                productImage: [...newProductImage]
+
+            }
+        })
+    }
 
 
     return (
@@ -63,7 +110,8 @@ const UploadProduct = ({
 
 
                     <label htmlFor='category' className='mt-3'>Category :</label>
-                    <select value={data.category} className='p-2 bg-slate-100 border rounded'>
+                    <select value={data.category} name='category' onChange={handleOnChange} className='p-2 bg-slate-100 border rounded'>
+                        <option value={""}>Select Product Category</option>
                         {
                             productCategory.map( (el, index)=>{
                                 return(
@@ -76,23 +124,97 @@ const UploadProduct = ({
 
                     <label htmlFor='productImage' className='mt-3'>Product Image :</label>
                     <label htmlFor="uploadImageInput">
-                        <div className='p-2 bg-slate-100 border rounded h-32 w-full flex justify-center items-center'>
+                        <div className='p-2 bg-slate-100 border rounded h-32 w-full flex justify-center items-center cursor-pointer'>
 
                             <div className='text-slate-500 flex justify-center items-center flex-col gap-2'>
                                 <span className='text-4xl'><FaCloudUploadAlt /></span>
                                 <p className='text-sm'>Upload Product Image</p>
-                                <input type='file' id='uploadImageInput' className='hidden'/>
+                                <input type='file' id='uploadImageInput' className='hidden' onChange={handleUploadProduct}/>
                             </div>
 
                         </div>
                     </label>
                     <div>
-                        <img src="" width={80} height={80} className='bg-slate-100 border'/>
+                        {
+                            data?.productImage[0] ? (
+                                <div className='flex items-center gap-2'>
+                                    {
+                                        data.productImage.map( (el, index)=>{
+                                            return(
+                                                <div className='relative group'>
+
+                                                    <img
+                                                        src={el}
+                                                        alt={el}
+                                                        width={80} 
+                                                        height={80} 
+                                                        className='bg-slate-100 border cursor-pointer'
+                                                        onClick={ ()=>{
+                                                            setOpenFullScreenImage(true)
+                                                            setFullScreenImage(el)
+                                                        }}
+                                                    />
+
+                                                    <div className='absolute bottom-0 right-0 p-1 text-white bg-red-600 rounded-full hidden group-hover:block cursor-pointer' onClick={ ()=>handleDeleteProductImage(index) }>
+                                                        <MdDelete/>
+                                                    </div>
+
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                            :
+                            (
+                                <p className='text-red-600 text-xs'>*Please upload product image</p>
+                            )
+                        }
+                        
                     </div>
+
+
+                    <label htmlFor='price' className='mt-3'>Price :</label>
+                    <input 
+                        type='number'
+                        id='price'
+                        placeholder='enter price'
+                        value={data.price}
+                        name='price'
+                        onChange={handleOnChange}
+                        className='p-2 bg-slate-100 border rounded'
+                    />
+
+
+                    <label htmlFor='sellingPrice' className='mt-3'>Selling Price :</label>
+                    <input 
+                        type='number'
+                        id='sellingPrice'
+                        placeholder='enter selling price'
+                        value={data.sellingPrice}
+                        name='sellingPrice'
+                        onChange={handleOnChange}
+                        className='p-2 bg-slate-100 border rounded'
+                    />
+
+
+                    <label htmlFor='description' className='mt-3'>Description :</label>
+                    <textarea className='h-28 bg-slate-100 border resize-none p-1' placeholder='enter product description' rows={3}></textarea>
+
+
+                    <button className='px-3 py-2 mb-10 bg-red-600 text-white hover:bg-red-700'>Upload Product</button>
 
                 </form>
 
             </div>
+
+
+            {/**Display full image on screen */}
+            {
+                openFullScreenImage && (
+                    <DisplayImage  onClose={ ()=>setOpenFullScreenImage(false) }  imgUrl={fullScreenImage}/>
+                )
+            }
 
         </div>
 

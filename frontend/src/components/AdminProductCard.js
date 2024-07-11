8b7abeb8e7485afad3fb72062676cccfcd4549ce
Sajid <mdsajidali1803@gmail.com>
2024-07-11 12:42:31 +0530

    Add updated file

--- frontend/src/components/AdminProductCard.js
+++ frontend/src/components/AdminProductCard.js
@@ -18,8 +18,11 @@ const AdminProductCard = ({
 
             <div className='w-40'>
                 
-                <img src={data?.productImage[0]} width={120} height={120} className='w-fit mx-auto'/>
-                <h1>{data.productName}</h1>
+                <div className='w-32 h-32 flex justify-center items-center'>
+                    <img src={data?.productImage[0]} className='mx-auto object-fill h-full'/>
+                </div>
+
+                <h1 className='text-ellipsis line-clamp-2'>{data.productName}</h1>
 
                 <div>
 

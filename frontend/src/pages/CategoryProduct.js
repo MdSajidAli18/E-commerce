485ec4fc10a6181eba6a38b08124729c3286aece
Sajid <mdsajidali1803@gmail.com>
2024-07-11 21:33:49 +0530

    Add files

---
+++ frontend/src/pages/CategoryProduct.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const CategoryProduct = () => {
+
+    const params = useParams()
+
+
+    
+    return (
+
+        <div>
+            {
+                params?.categoryName
+            }
+        </div>
+    )
+
+}
+
+export default CategoryProduct

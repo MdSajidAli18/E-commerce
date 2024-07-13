d9ee83488b905f0a5fb5d7df85915c931c449906
Sajid <mdsajidali1803@gmail.com>
2024-07-13 14:02:39 +0530

    Add updated files

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CategoryList from '../components/CategoryList';
 import BannerProduct from '../components/BannerProduct';
 import HorizontalCardProduct from '../components/HorizontalCardProduct';
+import VerticalCardProduct from '../components/VerticalCardProduct';
 
 const Home = () => {
   return (
@@ -11,6 +12,15 @@ const Home = () => {
       <HorizontalCardProduct  category={"airpodes"}  heading={"Top Airpodes"}/>
       <HorizontalCardProduct  category={"earphones"}  heading={"Best of Earphones"}/>
       <HorizontalCardProduct  category={"watches"}  heading={"Popular Watches"}/>
+      <VerticalCardProduct  category={"mobiles"}  heading={"Mobiles"}/>
+      <VerticalCardProduct  category={"televisions"}  heading={"Telivisions"}/>
+      <VerticalCardProduct  category={"speakers"}  heading={"Speakers"}/>
+      <VerticalCardProduct  category={"refrigerator"}  heading={"Refrigerators"}/>
+      <VerticalCardProduct  category={"trimmers"}  heading={"Trimmers"}/>
+      <VerticalCardProduct  category={"camera"}  heading={"Cameras"}/>
+      <VerticalCardProduct  category={"Mouse"}  heading={"Mouse"}/>
+      <VerticalCardProduct  category={"processor"}  heading={"Processors"}/>
+      <VerticalCardProduct  category={"printers"}  heading={"Printers"}/>
     </div>
   );
 };

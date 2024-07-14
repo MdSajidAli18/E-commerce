7da4ba46339a0e3b3bb05a3e41d25bc068b2f0d0
Sajid <mdsajidali1803@gmail.com>
2024-07-14 14:08:01 +0530

    Add files

---
+++ backend/controller/user/addToCartController.js
@@ -0,0 +1,49 @@
+const addToCartModel = require("../../models/cartProduct")
+
+
+const addToCartController = async(req, res)=>{
+
+    try {
+        
+        const { productId } = req?.body
+        const currentUser = req.userId
+
+        const isProductAlreadyInCart = await addToCartModel.findOne({productId})
+
+        if(isProductAlreadyInCart) {
+            return res.status(409).json({
+                message: "This product is already in the cart",
+                success: false,
+                error: true
+            })
+        }
+
+        const payload = {
+            productId: productId,
+            quantity: 1,
+            userId: currentUser
+        }
+
+        const newProductAddToCart = new addToCartModel(payload)
+        const saveProductToCart = await newProductAddToCart.save()
+
+        res.status(201).json({
+            message: "Product added to cart",
+            data: saveProductToCart,
+            success: true,
+            error: false
+        })
+
+
+    } catch (err) {
+        res.status(400).json({
+            message: err?.message || err,
+            error: true,
+            success: false
+        })
+    }
+
+}
+
+
+module.exports = addToCartController
---
+++ backend/models/cartProduct.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+const addToCartSchema = mongoose.Schema({
+    productId: String,
+    quantity: Number,
+    userId: String
+
+}, {timestamps: true})
+
+
+const addToCartModel = mongoose.model("addToCart", addToCartSchema)
+
+module.exports = addToCartModel

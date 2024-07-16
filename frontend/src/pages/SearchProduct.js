f7292548f68352bfab2b673486fa818b7be7e9e0
Sajid <mdsajidali1803@gmail.com>
2024-07-16 17:23:28 +0530

    Add updated file

--- frontend/src/pages/SearchProduct.js
+++ frontend/src/pages/SearchProduct.js
@@ -1,32 +1,61 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import SummaryApi from '../common';
+import noResultFoundImage from '../assets/noResultFoundImage.jpg'
 
 const SearchProduct = () => {
 
-    const query = useLocation()
-    console.log('Queryyy', query.search);
+  const query = useLocation()
+  console.log('Queryyy', query.search);
 
-    const fetchProduct = async()=>{
-        const response = await fetch(SummaryApi.searchProductt.url+query.search)
-        const detailsResponse = await response.json()
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-        console.log("Detailss Responsee", detailsResponse);
-    }
+  const fetchProduct = async()=>{
+
+    setLoading(true)
+
+    const response = await fetch(SummaryApi.searchProductt.url+query.search)
+    const dataResponse = await response.json()
+
+    setLoading(false)
+
+    setData(dataResponse.data)
+
+  }
 
 
-    useEffect(()=>{
-        fetchProduct()
-    }, [query])
+  useEffect(()=>{
+      fetchProduct()
+  }, [query])
 
 
 
   return (
-    <div>
-      SearchProduct
-      SearchProduct
+    <div className='container mx-auto p-4'>
+
+      {
+        loading && (
+          <p className='text-lg text-center font-semibold'>Page Loading...</p>
+        )
+      }
+
+      <p className='font-semibold text-lg'>Search Results : {data.length}</p>
+
+      {
+        !loading && data.length === 0 && (
+          
+          <div className='bg-white text-center p-40 gap-3 flex flex-col items-center justify-center w-full'>
+            <img src={noResultFoundImage} className=' w-60 h-60'/>
+            <p className=' text-3xl font-semibold'>Sorry, no results found!</p>
+            <p className='text-2xl text-ellipsis text-gray-500'>Please check the spelling or try searching for something else</p>
+          </div>
+        )
+      }
+
     </div>
   )
+
 }
 
 export default SearchProduct

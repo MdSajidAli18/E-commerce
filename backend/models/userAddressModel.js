bd94d8e0d705cf18c88f37174c344da71f9b5062
Sajid <mdsajidali1803@gmail.com>
2024-07-20 15:23:29 +0530

    Add file

---
+++ backend/models/userAddressModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipCode: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+});
+
+const addressModel = mongoose.model("Address", addressSchema);
+
+module.exports = addressModel;

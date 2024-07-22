50e299063ea23a1ea0d82cf3ab973179e6fcba51
Sajid <mdsajidali1803@gmail.com>
2024-07-22 12:44:16 +0530

    Add updated file

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser())
 
 app.use("/api", router)
 
-const PORT = 8080 || Process.env.PORT;
+const PORT = 8080 || process.env.PORT;
 
 connectDB()?.then(()=> {
     app.listen(PORT, () => {
